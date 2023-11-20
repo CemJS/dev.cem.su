b15d10158c2ba3ab24321375c57d511a033e7f58
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-11-20 18:22:34 +0300

    .

--- frontends/slider/display/Main.tsx
+++ frontends/slider/display/Main.tsx
@@ -9,12 +9,20 @@ export default function () {
                     products.map(item => {
                         return (
                             <div class="slide">
+                                <div class="slide-info">
+                                    <h5 class="slide-info_title">{item.title}</h5>
+                                    <p class="slide-info_desc">{item.desc}</p>
+                                    <a
+                                        link={item.urlAppStore}
+                                        class="btn btn__primary"
+                                    >
+                                        Подробнее
+                                    </a>
+                                </div>
 
-                                <h5>{item.title}</h5>
-                                <p>{item.desc}</p>
-                                <a link={item.urlAppStore}>Подробнее</a>
-
-                                {/* <img src={item.img} /> */}
+                                <div class="slide-img">
+                                    <img src={item.icon} alt={item.title} />
+                                </div>
                             </div>
                         )
                     })
